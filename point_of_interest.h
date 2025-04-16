d20012d98d54e9b16d435349cd5b010293bd9efc
Cyril Silapa <silapanenguemcyril@gmail.com>
2025-04-17 00:40:10 +0200

    Update point_of_interest.h

--- point_of_interest.h
+++ point_of_interest.h
@@ -1,22 +1,3 @@
-// #ifndef POINT_OF_INTEREST_H
-// #define POINT_OF_INTEREST_H
-// #include "ort.h"
-// #include <string>
-
-// class Point_of_interest : public Ort
-// {
-// private:
-//     string kategorie, bemerkung;
-// public:
-//     Point_of_interest(double lange, double breite, const string &kategorie, const string &bemerkung);
-//     Point_of_interest(int id, double lange, double breite, const string &kategorie, const string &bemerkung);
-//     string getKategorie() const;
-//     string getBemerkung() const;
-//     void anzeigen() const override;
-//     string toString() const override;
-// };
-
-// #endif // POINT_OF_INTEREST_H
 
 #ifndef POINT_OF_INTEREST_H
 #define POINT_OF_INTEREST_H
