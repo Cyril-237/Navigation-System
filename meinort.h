4b620630fb88406a6d815fb945d38126e76db5c1
Cyril Silapa <silapanenguemcyril@gmail.com>
2025-04-17 00:37:12 +0200

    Update meinort.h

--- meinort.h
+++ meinort.h
@@ -1,20 +1,4 @@
-// #ifndef MEINORT_H
-// #define MEINORT_H
 
-// #include "ort.h"
-// class MeinOrt : public Ort
-// {
-// public:
-//     MeinOrt();
-//     void anzeigen() const override;
-//     void verschieben(double neue_laenge, double neue_breite);
-//     // void meinort_anzeigen() const;
-//     void calculerDistanceAvecId(int id) const;
-//     string toString() const override;
-
-// };
-
-// #endif // MEINORT_H
 
 #ifndef MEINORT_H
 #define MEINORT_H
