216f121b57e2b2aed9172f8a7283a70ffd7a41f2
Cyril Silapa <silapanenguemcyril@gmail.com>
2025-04-16 21:28:04 +0200

    Update adresse.h

--- adresse.h
+++ adresse.h
@@ -1,27 +1,3 @@
-// #ifndef ADRESSE_H
-// #define ADRESSE_H
-// #include "ort.h"
-// #include <string>
-
-// class Adresse : public Ort
-// {
-// private:
-//     int hausnummer, postleitzahl;
-//     std::string straase, gemeinde, name;
-
-// public:
-//     Adresse(double lange, double breite, int hausnummer, const string& name, int postleitzahl, const string &straase, const string &gemeinde);
-//     Adresse(int id , double lange, double breite, const string& name ,int hausnummer, int postleitzahl, const string &straase, const string &gemeinde);
-//     int getHausnummer() const;
-//     int getPostleitzahl() const;
-//     std::string getStraase() const;
-//     std::string getGemeinde() const;
-//     void anzeigen() const override ;
-//     std::string getName() const;
-//     string toString() const override;
-// };
-
-// #endif // ADRESSE_H
 
 #ifndef ADRESSE_H
 #define ADRESSE_H
